bc85503e19cab5b998fa7146bce0ca3f9ee80c6a
BFronz <bfronz1960@me.com>
2020-05-14 08:25:28 -0400

    add country bar chart

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useState, useEfect, useEffect} from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
 
-const CountryPicker = () => {
+import styles  from './CountryPicker.module.css';
+
+
+import { fetchCountries } from '../../api';
+
+
+const CountryPicker = ({handleCountryChange}) => {
+    const[fetchedCountries, setFetchedCountries] = useState([]);
+
+
+useEffect ( () => {
+
+    const fetchAPI = async () => {
+        setFetchedCountries(await fetchCountries());
+    }
+
+    fetchAPI();
+},[setFetchedCountries]);
+    
+console.log(fetchedCountries);
+
     return (
-        <hi>CountryPicker</hi>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={(event) => handleCountryChange(event.target.value)}>
+                <option value="global">Global</option>        
+    {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>  )}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
